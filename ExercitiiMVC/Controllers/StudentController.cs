e02369c0ce7802ef1a437e73ff83527b158c25f9
sebastian popescu <sebastianh123@yahoo.com>
2020-01-21 18:07:25 +0200

    a

---
+++ ExercitiiMVC/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExercitiiMVC.Models;
+
+namespace ExercitiiMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentaData studentaData;
+        public StudentController()
+        {
+            this.studentaData = new StudentaData();
+        }
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> lista = studentaData.GetStudents();
+            return this.View(lista);
+        }
+        public ActionResult Create(Student student)
+        {
+
+            if(student.FirstName!= null && student.LastName!=null)
+            {
+                studentaData.AddStudent(student);
+                
+            }
+            
+            return View();
+           
+        }
+        
+        
+    }
+}
---
+++ ExercitiiMVC/Models/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExercitiiMVC.Models
+{
+    public class Student
+    {
+        public int Age { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class StudentaData
+    {
+        private static readonly List<Student> students = new List<Student>();
+        public StudentaData() { }
+        static StudentaData()
+        {
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+            students.Add(new Student { Age = 30, FirstName = "ion", LastName = "vaile" });
+
+        }
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+        
+
+
+
+    }
+}
